fd8798a06ba40db12d31bd88de095f4bc2baf185
Josh Mullin <jmull0@protonmail.com>
2020-05-27 20:45:46 +1200

    created file for search results adapter

--- app/src/main/java/com/sthoray/allright/SearchResultsActivity.kt
+++ app/src/main/java/com/sthoray/allright/SearchResultsActivity.kt
@@ -1,10 +1,8 @@
 package com.sthoray.allright
 
-import android.graphics.Color
+import SearchResultsAdapter
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,28 +11,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 class SearchResultsActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         super.setContentView(R.layout.activity_main)
 
         recyclerView_topLevel.layoutManager = LinearLayoutManager(this)
         recyclerView_topLevel.adapter = SearchResultsAdapter()
     }
-    private class SearchResultsAdapter: RecyclerView.Adapter<SearchResultsViewHolder>(){
-        override fun getItemCount(): Int {
-            return 20
-        }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchResultsViewHolder {
-            val layoutInflater = LayoutInflater.from(parent.context)
-            val view = layoutInflater.inflate(R.layout.search_results_row, parent, false)
-            return SearchResultsViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: SearchResultsViewHolder, position: Int) {
-
-        }
-    }
-    private class SearchResultsViewHolder(val searchResultsView : View) : RecyclerView.ViewHolder(searchResultsView){
-
-    }
+    class SearchResultsViewHolder(val searchResultsView : View) : RecyclerView.ViewHolder(searchResultsView)
 }
--- app/src/main/java/com/sthoray/allright/SearchResultsAdapter.kt
+++ app/src/main/java/com/sthoray/allright/SearchResultsAdapter.kt
@@ -1,2 +1,21 @@
-package com.sthoray.allright
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sthoray.allright.R
+import com.sthoray.allright.SearchResultsActivity
 
+class SearchResultsAdapter: RecyclerView.Adapter<SearchResultsActivity.SearchResultsViewHolder>(){
+    override fun getItemCount(): Int {
+        return 20
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchResultsActivity.SearchResultsViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.search_results_row, parent, false)
+        return SearchResultsActivity.SearchResultsViewHolder(view)
+    }
+//
+    override fun onBindViewHolder(holder: SearchResultsActivity.SearchResultsViewHolder, position: Int) {
+
+    }
+}
