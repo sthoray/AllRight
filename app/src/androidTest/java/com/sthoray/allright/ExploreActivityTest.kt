d86f96d8bafef8f874bdd3bcb4e29ebccc09ce37
benjamin <benjamin.mccarthy.nz@gmail.com>
2020-05-31 21:53:07 +1200

    Added an ID to the activity_search.xml file. Also created a test for checking that clicking a category navigates to the correct activity. This is not working properly yet

--- app/src/androidTest/java/com/sthoray/allright/ExploreActivityTest.kt
+++ app/src/androidTest/java/com/sthoray/allright/ExploreActivityTest.kt
@@ -3,6 +3,7 @@ package com.sthoray.allright
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -24,19 +25,23 @@ class ExploreActivityTest{
     fun test_isActivityInView() {
 
         //This function launches the activity and tests that it is in view
-
-        //val activityScenario = ActivityScenario.launch(ExploreActivity::class.java)
-
         onView(withId(R.id.exploreActivity)).check(matches(isDisplayed()))
     }
 
     @Test
     fun test_visibility_categories() {
-        //This function test the visibility of the categories
-
-        //val activityScenario = ActivityScenario.launch(ExploreActivity::class.java)
-
+        //This function tests the visibility of the categories
         onView(withId(R.id.recyclerView_featuredCategories))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_navSearchActivity() {
+        //This function tests the navigation to the SearchActivity
+        //Not quite working yet
+        onView(withId(R.id.recyclerView_featuredCategories)).perform(click())
+
+        onView(withId(R.id.searchActivity))
             .check(matches(isDisplayed()))
     }
 }
