f05f41af36ff5bc6ff9ebfdae9d398e4766e9c16
benjamin <benjamin.mccarthy.nz@gmail.com>
2020-05-31 21:38:43 +1200

    Changed the launching of the activity to be a global variable instead of re-declaring it in each function

--- app/src/androidTest/java/com/sthoray/allright/ExploreActivityTest.kt
+++ app/src/androidTest/java/com/sthoray/allright/ExploreActivityTest.kt
@@ -6,13 +6,18 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class ExploreActivityTest{
+
+    @get: Rule
+    val activityRule = ActivityScenarioRule(ExploreActivity::class.java)
 
 
     @Test
@@ -20,7 +25,7 @@ class ExploreActivityTest{
 
         //This function launches the activity and tests that it is in view
 
-        val activityScenario = ActivityScenario.launch(ExploreActivity::class.java)
+        //val activityScenario = ActivityScenario.launch(ExploreActivity::class.java)
 
         onView(withId(R.id.exploreActivity)).check(matches(isDisplayed()))
     }
@@ -29,7 +34,7 @@ class ExploreActivityTest{
     fun test_visibility_categories() {
         //This function test the visibility of the categories
 
-        val activityScenario = ActivityScenario.launch(ExploreActivity::class.java)
+        //val activityScenario = ActivityScenario.launch(ExploreActivity::class.java)
 
         onView(withId(R.id.recyclerView_featuredCategories))
             .check(matches(isDisplayed()))
