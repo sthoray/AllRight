f6ce2500f8587c8e5fe5f064f3a92c36995f7dc4
Josh <jmull0@protonmail.com>
2020-07-16 12:15:23 +1200

    Added variables to enum SortOrder.kt

--- app/src/main/java/com/sthoray/allright/utils/SortOrder.kt
+++ app/src/main/java/com/sthoray/allright/utils/SortOrder.kt
@@ -8,5 +8,15 @@ package com.sthoray.allright.utils
 enum class SortOrder(val key: String) {
 
     /** Sort listings by best match. */
-    BEST("best_match")
+    BEST("best_match"),
+    TRENDING("most_popular"),
+    NEW_LISTINGS("created_at"),
+    CLOSING_SOON("closing_soon"),
+    MOST_BIDS("most_bids"),
+    BUY_NOW_HIGHEST_FIRST("highest_buy_now"),
+    BUY_NOW_LOWEST_FIRST("lowest_buy_now"),
+    PRICE_LOWEST_FIRST("lowest_to_highest"),
+    PRICE_HIGHEST_FIRST("highest_to_lowest"),
+    A_TO_Z("name")
+
 }
