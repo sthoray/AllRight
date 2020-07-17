ec7e948522fa97ae54191b324343dd0869074952
Josh Mullin <jmull0@protonmail.com>
2020-07-17 15:37:39 +1200

    Indent comment

--- app/src/main/java/com/sthoray/allright/data/model/search/SearchRequest.kt
+++ app/src/main/java/com/sthoray/allright/data/model/search/SearchRequest.kt
@@ -37,8 +37,8 @@ data class SearchRequest(
     var freeShipping: Int = 0,
     var location: Any = Any(),
     @SerializedName("max_price")
-    var maxPrice: Float? = null,   // Client side casts max price and min price to an int
-                             // but the server side accepts float values
+    var maxPrice: Float? = null,    // Client side casts max price and min price to an int
+                                    // but the server side accepts float values
     @SerializedName("min_price")
     var minPrice: Float? = null,
     @SerializedName("page")
