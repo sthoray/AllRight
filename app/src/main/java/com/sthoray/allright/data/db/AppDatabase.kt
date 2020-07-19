3eed6c46786f0fa0fe905c647f363f39f8172f59
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-07-20 02:06:20 +1200

    Clean up imports

--- app/src/main/java/com/sthoray/allright/data/db/AppDatabase.kt
+++ app/src/main/java/com/sthoray/allright/data/db/AppDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.sthoray.allright.data.model.main.FeatureCategory
 import com.sthoray.allright.data.model.search.Listing
 
 /**
