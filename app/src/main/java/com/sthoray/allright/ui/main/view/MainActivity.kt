195d2e2b6ed43f22c7c50001d77d8db28f89fa05
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-07-20 20:13:16 +1200

    Simplified setup of navigation bar

--- app/src/main/java/com/sthoray/allright/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/sthoray/allright/ui/main/view/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.sthoray.allright.ui.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sthoray.allright.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * The main activity where everything starts.
  *
- * This is the "home page" where the user us presented with suggestions to
- * start searching and can view their past search queries.
+ * Contains a bottom navigation bar to switch between commonly
+ * used fragments.
  */
 class MainActivity : AppCompatActivity() {
 
@@ -33,12 +31,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.navigationViewMain)
-        val navController = findNavController(R.id.navHostFragmentMain)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_browse, R.id.navigation_profile
-        ))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        bottomNavigationView.setupWithNavController(
+            navigationHostFragment.findNavController()
+        )
     }
 }
