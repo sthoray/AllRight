572e401ef3f6fef176b9e495748252b125eba064
shaus019 <shaus019@student.otago.ac.nz>
2020-07-22 00:28:11 +1200

    Restored the util package which i deleted by mistake.

---
+++ app/src/main/java/com/sthoray/allright/utils/SortOrder.kt
@@ -0,0 +1,19 @@
+package com.sthoray.allright.utils
+
+/**
+ * The order to return search listings.
+ *
+ * @property key the AllGoods API sort key
+ */
+enum class SortOrder(val key: String) {
+    BEST("best_match"),
+    TRENDING("most_popular"),
+    NEW_LISTINGS("created_at"),
+    CLOSING_SOON("closing_soon"),
+    MOST_BIDS("most_bids"),
+    BUY_NOW_HIGHEST_FIRST("highest_buy_now"),
+    BUY_NOW_LOWEST_FIRST("lowest_buy_now"),
+    PRICE_LOWEST_FIRST("lowest_to_highest"),
+    PRICE_HIGHEST_FIRST("highest_to_lowest"),
+    A_TO_Z("name")
+}
