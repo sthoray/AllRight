7c772d4ea3ec71bb0f6d1ff7e13d905d50caa917
Josh Mullin <jmull0@protonmail.com>
2020-07-23 19:59:04 +1200

    remove hasInternetConnection() from ViewModels #71

--- app/src/main/java/com/sthoray/allright/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sthoray/allright/ui/main/viewmodel/MainViewModel.kt
@@ -101,34 +101,5 @@ class MainViewModel(
         }
         return Resource.Error(response.message())
     }
-    private fun hasInternetConnection() : Boolean {
-        val connectivityManager = getApplication<Application>().getSystemService(
-            Context.CONNECTIVITY_SERVICE
-        ) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            val activeNetwork = connectivityManager.activeNetwork ?: return false
-            val capabilities = connectivityManager
-                .getNetworkCapabilities(activeNetwork) ?: return false
-            return when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
-        } else {
-            /** TODO
-             * remove use of deprecated without removing internet
-             * checking for devices pre Marshmallow (API 23)
-             */
-            connectivityManager.activeNetworkInfo?.run {
-                return when(type){
-                    ConnectivityManager.TYPE_WIFI -> true
-                    ConnectivityManager.TYPE_MOBILE -> true
-                    ConnectivityManager.TYPE_ETHERNET -> true
-                    else -> false
-                }
-            }
-        }
-        return false
-    }
+    
 }
--- app/src/main/java/com/sthoray/allright/ui/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/sthoray/allright/ui/search/viewmodel/SearchViewModel.kt
@@ -82,34 +82,5 @@ class SearchViewModel(
             }
         }
     }
-    private fun hasInternetConnection() : Boolean {
-        val connectivityManager = getApplication<Application>().getSystemService(
-            Context.CONNECTIVITY_SERVICE
-        ) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            val activeNetwork = connectivityManager.activeNetwork ?: return false
-            val capabilities = connectivityManager
-                .getNetworkCapabilities(activeNetwork) ?: return false
-            return when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
-        } else {
-            /** TODO
-             * remove use of deprecated without removing internet
-             * checking for devices pre Marshmallow (API 23)
-             */
-            connectivityManager.activeNetworkInfo?.run {
-                return when(type){
-                    TYPE_WIFI -> true
-                    TYPE_MOBILE -> true
-                    TYPE_ETHERNET -> true
-                    else -> false
-                }
-            }
-        }
-        return false
-    }
+
 }
