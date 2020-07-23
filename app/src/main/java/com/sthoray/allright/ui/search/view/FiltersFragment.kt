33c13997187d37214d5e091ee8bcc45e26032264
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-07-23 17:19:45 +1200

    Create filters fragment files and update the search nav graph

---
+++ app/src/main/java/com/sthoray/allright/ui/search/view/FiltersFragment.kt
@@ -0,0 +1,15 @@
+package com.sthoray.allright.ui.search.view
+
+import androidx.fragment.app.Fragment
+import com.sthoray.allright.R
+
+/**
+ * Search filtering fragment.
+ *
+ * This fragment provides a central place to view, modify, and remove all
+ * filters for the current search request.
+ */
+class FiltersFragment : Fragment(R.layout.fragment_filters) {
+
+    // TODO: Implement fragment
+}
