d3ed33d1764727fb1eade46ec822777f23ce5955
Josh Mullin <jmull0@protonmail.com>
2020-07-25 22:00:13 +1200

    remove import #71

--- app/src/main/java/com/sthoray/allright/utils/Internet.kt
+++ app/src/main/java/com/sthoray/allright/utils/Internet.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.lifecycle.AndroidViewModel
 
 /**
  * Internet utils class
