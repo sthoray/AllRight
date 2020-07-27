8b58c48ada1f6671c4cecc76b58067378c697d30
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-07-28 11:46:43 +1200

    Clean up + remove carousel view and picasso

--- app/src/main/java/com/sthoray/allright/ui/listing/view/ListingActivity.kt
+++ app/src/main/java/com/sthoray/allright/ui/listing/view/ListingActivity.kt
@@ -1,15 +1,11 @@
 package com.sthoray.allright.ui.listing.view
 
-import android.icu.text.AlphabeticIndex
 import android.os.Bundle
-import android.widget.ImageView
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import coil.api.load
-import com.squareup.picasso.Picasso
 import com.sthoray.allright.R
 import com.sthoray.allright.data.db.SearchHistoryDatabase
 import com.sthoray.allright.data.model.listing.Image
@@ -17,8 +13,6 @@ import com.sthoray.allright.data.repository.AppRepository
 import com.sthoray.allright.ui.base.ViewModelProviderFactory
 import com.sthoray.allright.ui.listing.viewmodel.ListingViewModel
 import com.sthoray.allright.ui.search.view.SearchActivity.Companion.LISTING_ID_KEY
-import com.synnapps.carouselview.CarouselView
-import com.synnapps.carouselview.ImageListener
 import com.sthoray.allright.utils.Resource
 import kotlinx.android.synthetic.main.activity_listing.*
 
@@ -26,33 +20,25 @@ import kotlinx.android.synthetic.main.activity_listing.*
 class ListingActivity : AppCompatActivity() {
 
 
-    private lateinit var viewModel: ListingViewModel
     private val TAG = "ListingActivity"
-
-    var imageUrls: List<String> = listOf()
-
+    private lateinit var viewModel: ListingViewModel
+    private lateinit var imageUrls: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listing)
         setupViewModel()
-        viewModel.getListing(
-            intent.getIntExtra(
-                LISTING_ID_KEY,
-                0
-            )
-
-        )
         setupObservers()
 
-        // TODO: Open website as follows
+        val listingId = intent.getIntExtra(LISTING_ID_KEY, 0)
+        viewModel.getListing(listingId)
+
+        // TODO: Open website
         /*resultsAdapter.setOnItemClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(Constants.BASE_PRODUCT_URL + it.id)
             this.startActivity(intent)
         } */
-
-
     }
 
     private fun setupViewModel() {
@@ -70,15 +56,7 @@ class ListingActivity : AppCompatActivity() {
                     response.data?.let { listing ->
                         textViewTitle.text = listing.productName
                         textViewDescription.text = listing.description
-                        imageUrls = createImageUrlList(listing.images)
-                        Log.d(TAG, imageUrls.toString())
-                        carouselView.apply {
-
-                            pageCount = imageUrls.size
-                            setImageListener(imageListener)
-
-
-                        }
+                        imageUrls = getImageUrls(listing.images)
                     }
                 }
                 is Resource.Error -> {
@@ -94,8 +72,8 @@ class ListingActivity : AppCompatActivity() {
         })
     }
 
-    private fun createImageUrlList(images: List<Image>): ArrayList<String> {
-        val imageList: ArrayList<String> = arrayListOf()
+    private fun getImageUrls(images: List<Image>): List<String> {
+        val imageList = mutableListOf<String>()
         for (image in images) {
             imageList.add(image.largeUrl)
         }
@@ -109,13 +87,4 @@ class ListingActivity : AppCompatActivity() {
     private fun hideProgressBar() {
         progBarListing.visibility = View.GONE
     }
-
-
-
-    var imageListener: ImageListener = object : ImageListener {
-        override fun setImageForPosition(position: Int, imageView: ImageView) {
-            Picasso.get().load(imageUrls[position]).into(imageView)
-        }
-    }
-
 }
