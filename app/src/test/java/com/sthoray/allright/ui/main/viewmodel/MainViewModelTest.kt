da734647e6f27f74f5ab7b449c23b1f953f6b949
Josh Mullin <jmull0@protonmail.com>
2020-08-03 13:52:03 +1200

    Add TestCoroutineDispatcher #78
    Signed-off-by: Josh Mullin <jmull0@protonmail.com>

--- app/src/test/java/com/sthoray/allright/ui/main/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/sthoray/allright/ui/main/viewmodel/MainViewModelTest.kt
@@ -1,34 +1,35 @@
 package com.sthoray.allright.ui.main.viewmodel
 
 import android.app.Application
-import androidx.test.core.app.ActivityScenario.launch
 import com.sthoray.allright.data.repository.AppRepository
 import io.mockk.mockk
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import kotlin.coroutines.CoroutineContext
 
 class MainViewModelTest {
+    @ExperimentalCoroutinesApi
+    private val testDispatcher = TestCoroutineDispatcher()
     private val application = mockk<Application>()
     private val appRepository = mockk<AppRepository>()
-    private val coroutineDispatcherImplementation = object : CoroutineDispatcher(){
-        override fun dispatch(context: CoroutineContext, block: Runnable) {
-
-        }
-
-
-    }
     private lateinit var mainViewModel: MainViewModel
+    @ExperimentalCoroutinesApi
     @Before
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
         mainViewModel = MainViewModel(application, appRepository)
 
     }
 
+    @ExperimentalCoroutinesApi
     @After
     fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
