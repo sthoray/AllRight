313302ba8c347d0aa7ad7b2d821beea6d08507fb
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-08-05 10:32:07 +1200

    WIP: Test all `ListingViewModel` paths except withResponseError #78

--- app/src/test/java/com/sthoray/allright/ui/listing/viewmodel/ListingViewModelTest.kt
+++ app/src/test/java/com/sthoray/allright/ui/listing/viewmodel/ListingViewModelTest.kt
@@ -3,6 +3,7 @@ package com.sthoray.allright.ui.listing.viewmodel
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
+import com.google.gson.JsonSyntaxException
 import com.sthoray.allright.data.model.listing.Listing
 import com.sthoray.allright.data.repository.AppRepository
 import com.sthoray.allright.utils.Internet
@@ -17,6 +18,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 import retrofit2.Response
+import java.io.IOException
 
 @ExperimentalCoroutinesApi
 class ListingViewModelTest {
@@ -43,7 +45,7 @@ class ListingViewModelTest {
     }
 
     @Test
-    fun getListing_withSuccessfulResponse_setsResourceSuccess() =
+    fun getListing_withResponseSuccess_setsResourceSuccess() =
         mainCoroutineRule.runBlockingTest {
             val testId = 9
 
@@ -57,5 +59,75 @@ class ListingViewModelTest {
 
             assertThat(listingViewModel.listing.value).isInstanceOf(Resource.Success::class.java)
             assertThat(listingViewModel.listing.value?.data).isEqualTo(listing)
+        }
+
+    @Test
+    fun getListing_withResponseError_setsResourceError() =
+        mainCoroutineRule.runBlockingTest {
+            val testId = 9
+
+            every { Internet.hasConnection(any()) } returns true
+            TODO("Mock a Response.error")
+            // coEvery { appRepository.getListing(eq(testId)) } returns Response.error()
+
+            val listingViewModel = ListingViewModel(app, appRepository)
+            listingViewModel.getListing(testId)
+
+            verify { Internet.hasConnection(any()) }
+
+            assertThat(listingViewModel.listing.value).isInstanceOf(Resource.Error::class.java)
+            TODO("Verify the correct error message is contained")
+            //assertThat(listingViewModel.listing.value?.message).isEqualTo(listing)
+        }
+
+    @Test
+    fun getListing_withoutNetworkConnection_setsResourceError() =
+        mainCoroutineRule.runBlockingTest {
+            val testId = 9
+
+            every { Internet.hasConnection(any()) } returns false
+
+            val listingViewModel = ListingViewModel(app, appRepository)
+            listingViewModel.getListing(testId)
+
+            verify { Internet.hasConnection(any()) }
+            coVerify(exactly = 0) { appRepository.getListing(any()) }
+
+            assertThat(listingViewModel.listing.value).isInstanceOf(Resource.Error::class.java)
+            assertThat(listingViewModel.listing.value?.message).isEqualTo("No internet connection")
+        }
+
+    @Test
+    fun getListing_withNetworkFailure_setsResourceError() =
+        mainCoroutineRule.runBlockingTest {
+            val testId = 9
+
+            every { Internet.hasConnection(any()) } returns true
+            coEvery { appRepository.getListing(eq(testId)) } throws IOException()
+
+            val listingViewModel = ListingViewModel(app, appRepository)
+            listingViewModel.getListing(testId)
+
+            verify { Internet.hasConnection(any()) }
+
+            assertThat(listingViewModel.listing.value).isInstanceOf(Resource.Error::class.java)
+            assertThat(listingViewModel.listing.value?.message).isEqualTo("Network Failure")
+        }
+
+    @Test
+    fun getListing_withConversionError_setsResourceError() =
+        mainCoroutineRule.runBlockingTest {
+            val testId = 9
+
+            every { Internet.hasConnection(any()) } returns true
+            coEvery { appRepository.getListing(eq(testId)) } throws JsonSyntaxException("Mockk exception message")
+
+            val listingViewModel = ListingViewModel(app, appRepository)
+            listingViewModel.getListing(testId)
+
+            verify { Internet.hasConnection(any()) }
+
+            assertThat(listingViewModel.listing.value).isInstanceOf(Resource.Error::class.java)
+            assertThat(listingViewModel.listing.value?.message).isEqualTo("Conversion Error")
         }
 }
