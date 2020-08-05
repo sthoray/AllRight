d477b9bcbe68a2ee692437b5f0d094c912755a39
Josh Mullin <jmull0@protonmail.com>
2020-08-05 20:32:02 +1200

    getFeaturedCategoriesSuccessful()

--- app/src/test/java/com/sthoray/allright/ui/main/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/sthoray/allright/ui/main/viewmodel/MainViewModelTest.kt
@@ -2,18 +2,24 @@ package com.sthoray.allright.ui.main.viewmodel
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.sthoray.allright.data.model.listing.Listing
+import com.google.common.truth.Truth.assertThat
+import com.sthoray.allright.data.model.main.FeatureCategoriesResponse
 import com.sthoray.allright.data.repository.AppRepository
 import com.sthoray.allright.utils.Internet
+import com.sthoray.allright.utils.Resource
 import com.sthoray.allright.utils.TestCoroutineRule
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.mockkObject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TestRule
+import retrofit2.Response
 
 @ExperimentalCoroutinesApi
 class MainViewModelTest {
@@ -31,11 +37,23 @@ class MainViewModelTest {
     private lateinit var appRepository: AppRepository
 
     @RelaxedMockK
-    private lateinit var listing: Listing
+    private lateinit var featureCategoriesResponse: FeatureCategoriesResponse
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
         mockkObject(Internet)
+    }
+    @Test
+    fun getFeaturedCategoriesSuccessful(){
+        every { Internet.hasConnection(any()) } returns true
+        coEvery {
+            appRepository.getFeatureCategories()
+        } returns Response.success(featureCategoriesResponse)
+
+        val mainViewModel = MainViewModel(app, appRepository)
+
+        assertThat(mainViewModel.featureCategories.value)
+            .isInstanceOf(Resource.Success::class.java)
     }
 }
