da5550aed9fb39c9e2f4c66215900d21d211c6d2
Josh Mullin <jmull0@protonmail.com>
2020-08-07 13:47:41 +1200

    searchListings all cases tested #78

--- app/src/test/java/com/sthoray/allright/ui/search/viewmodel/SearchViewModelTest.kt
+++ app/src/test/java/com/sthoray/allright/ui/search/viewmodel/SearchViewModelTest.kt
@@ -2,12 +2,12 @@ package com.sthoray.allright.ui.search.viewmodel
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.google.common.truth.Truth
 import com.google.common.truth.Truth.assertThat
+import com.google.gson.JsonSyntaxException
 import com.sthoray.allright.data.model.search.SearchRequest
 import com.sthoray.allright.data.model.search.SearchResponse
 import com.sthoray.allright.data.repository.AppRepository
-import com.sthoray.allright.ui.listing.viewmodel.ListingViewModel
+import com.sthoray.allright.ui.main.viewmodel.MainViewModel
 import com.sthoray.allright.utils.Internet
 import com.sthoray.allright.utils.Resource
 import com.sthoray.allright.utils.TestCoroutineRule
@@ -15,6 +15,8 @@ import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.After
 import org.junit.Before
 
@@ -22,6 +24,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 import retrofit2.Response
+import java.io.IOException
 
 @ExperimentalCoroutinesApi
 class SearchViewModelTest {
@@ -86,6 +89,62 @@ class SearchViewModelTest {
                 .isInstanceOf(Resource.Error::class.java)
             assertThat(searchViewModel.searchListings.value?.message)
                 .isEqualTo("No internet connection")
+        }
+    @Test
+    fun searchListingsErrorNetworkFailure() =
+        mainCoroutineRule.runBlockingTest {
+            every { Internet.hasConnection(any()) } returns true
+            coEvery {
+                appRepository.searchListings(searchRequest)
+            } throws IOException()
+
+            val searchViewModel = SearchViewModel(app, appRepository)
+            searchViewModel.searchRequest = searchRequest
+            searchViewModel.searchListings()
+            verify { Internet.hasConnection(any()) }
+
+            assertThat(searchViewModel.searchListings.value)
+                .isInstanceOf(Resource.Error::class.java)
+            assertThat(searchViewModel.searchListings.value?.message)
+                .isEqualTo("Network Failure")
+        }
+    @Test
+    fun searchListingsErrorConversion() =
+        mainCoroutineRule.runBlockingTest {
+            every { Internet.hasConnection(any()) } returns true
+            coEvery {
+                appRepository.searchListings(searchRequest)
+            } throws JsonSyntaxException("Mockk Exception Message")
+
+            val searchViewModel = SearchViewModel(app, appRepository)
+            searchViewModel.searchRequest = searchRequest
+            searchViewModel.searchListings()
+            verify { Internet.hasConnection(any()) }
+
+            assertThat(searchViewModel.searchListings.value)
+                .isInstanceOf(Resource.Error::class.java)
+            assertThat(searchViewModel.searchListings.value?.message)
+                .isEqualTo("Conversion Error")
+        }
+    @Test
+    fun getListingsErrorSetsResourceError() =
+        mainCoroutineRule.runBlockingTest {
+            val errorResponse: Response<SearchResponse> = Response.error(
+                400,
+                "{\"key\":[\"some_stuff\"]}"
+                    .toResponseBody("application/json".toMediaTypeOrNull())
+            )
+
+            every { Internet.hasConnection(any()) } returns true
+            coEvery { appRepository.searchListings(searchRequest) } returns errorResponse
+
+            val searchViewModel = SearchViewModel(app, appRepository)
+            searchViewModel.searchRequest = searchRequest
+            searchViewModel.searchListings()
+            verify { Internet.hasConnection(any()) }
+
+            assertThat(searchViewModel.searchListings.value).isInstanceOf(Resource.Error::class.java)
+            assertThat(searchViewModel.searchListings.value?.message).isEqualTo(errorResponse.message())
         }
     @After
     fun tearDown() {
