8f6a8e7c209b155c2da435545760216c7f13e0cb
Josh <jmull0@protonmail.com>
2020-08-08 19:44:12 +1200

    Replaced assertThat for HTTP 200s with verify RetrofitInstance api function calls

--- app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
+++ app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
@@ -1,62 +1,88 @@
 package com.sthoray.allright.data.repository
 
-import com.google.common.truth.Truth.assertThat
+import com.sthoray.allright.data.api.RetrofitInstance
 import com.sthoray.allright.data.db.SearchHistoryDatabase
+import com.sthoray.allright.data.model.listing.Category
+import com.sthoray.allright.data.model.listing.Listing
+import com.sthoray.allright.data.model.main.FeatureCategoriesResponse
 import com.sthoray.allright.data.model.search.SearchRequest
+import com.sthoray.allright.data.model.search.SearchResponse
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockkObject
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Response
 
 class AppRepositoryTest {
 
     @MockK
     private lateinit var searchHistoryDatabase: SearchHistoryDatabase
 
+    @RelaxedMockK
+    private lateinit var featureCategoriesResponse: FeatureCategoriesResponse
+
+    @RelaxedMockK
+    private lateinit var secondTierCategoriesResponse: List<Category>
+
+    @RelaxedMockK
+    private lateinit var searchForListingsResponse: SearchResponse
+
+    @RelaxedMockK
+    private lateinit var getListingResponse: Listing
+
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
+        mockkObject(RetrofitInstance)
     }
 
-    /**
-     * TODO replace assertThat(response.code()).isEqualTo(200)
-     *  with coVerify { RetrofitInstance.api.functionName() }
-     *  for each function
-     *  ...
-     *  The Retrofit Instance test already tests the response code
-     *
-     */
     @Test
     fun getFeatureCategories() =
         runBlocking {
             val appRepository = AppRepository(searchHistoryDatabase)
-            val response = appRepository.getFeatureCategories()
-            assertThat(response.code()).isEqualTo(200)
+            val response = Response.success(featureCategoriesResponse)
+
+            coEvery { RetrofitInstance.api.getFeatureCategories() } returns response
+            appRepository.getFeatureCategories()
+            coVerify { RetrofitInstance.api.getFeatureCategories() }
         }
 
     @Test
     fun testGetSecondTierCategories() = runBlocking {
         val appRepository = AppRepository(searchHistoryDatabase)
-        val response = appRepository.getSecondTierCategories()
-        assertThat(response.code()).isEqualTo(200)
+        val response = Response.success(secondTierCategoriesResponse)
+
+        coEvery { RetrofitInstance.api.getSecondTierCategories() } returns response
+        appRepository.getSecondTierCategories()
+        coVerify { RetrofitInstance.api.getSecondTierCategories() }
     }
 
     @Test
     fun testSearchForListings() = runBlocking {
         val searchRequest = SearchRequest()
         val appRepository = AppRepository(searchHistoryDatabase)
-        val response = appRepository.searchListings(searchRequest)
-        assertThat(response.code()).isEqualTo(200)
+        val response = Response.success(searchForListingsResponse)
+
+
+        coEvery { RetrofitInstance.api.searchForListings(searchRequest) } returns response
+        appRepository.searchListings(searchRequest)
+        coVerify { RetrofitInstance.api.searchForListings(searchRequest) }
     }
 
     @Test
     fun testGetListing() = runBlocking {
         val listingId = 1234
         val appRepository = AppRepository(searchHistoryDatabase)
-        val response = appRepository.getListing(listingId)
-        assertThat(response.code()).isEqualTo(200)
-    }
+        val response = Response.success(getListingResponse)
 
+        coEvery { RetrofitInstance.api.getListing(listingId) } returns response
+        appRepository.getListing(listingId)
+        coVerify { RetrofitInstance.api.getListing(listingId) }
+    }
 
 }
