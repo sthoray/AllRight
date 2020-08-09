f8e935ce197bd9bb0a1ef90db1645d8fa3fca1da
Usman Shah <shaus019@student.otago.ac.nz>
2020-08-09 23:38:39 +1200

    added a test for searchlisting

--- app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
+++ app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
@@ -6,6 +6,7 @@ import com.sthoray.allright.data.db.SearchHistoryDatabase
 import com.sthoray.allright.data.model.listing.Category
 import com.sthoray.allright.data.model.listing.Listing
 import com.sthoray.allright.data.model.main.FeatureCategoriesResponse
+import com.sthoray.allright.data.model.search.SearchRequest
 import com.sthoray.allright.data.model.search.SearchResponse
 import com.sthoray.allright.utils.TestCoroutineRule
 import io.mockk.MockKAnnotations
@@ -69,7 +70,14 @@ class AppRepositoryTest {
 
 
     @Test
-    fun searchListings() {
+    fun searchListings() = runBlocking{
+        val appRepository = AppRepository(historyDatabase)
+        val response = Response.success(searchListingResponse)
+        val searchRequest = SearchRequest()
+
+        coEvery { RetrofitInstance.api.searchForListings()} returns response
+        appRepository.searchListings(searchRequest)
+        coVerify { RetrofitInstance.api.searchForListings() }
     }
 
     @Test
