f1c85ad4376fde9a168ea547f064dc2b283d2be4
Usman Shah <shaus019@student.otago.ac.nz>
2020-08-09 20:37:13 +1200

    added a method to test getFeaturedCategories

--- app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
+++ app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
@@ -1,16 +1,36 @@
 package com.sthoray.allright.data.repository
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth
+import com.sthoray.allright.data.api.RetrofitInstance
 import com.sthoray.allright.data.db.SearchHistoryDatabase
 import com.sthoray.allright.data.model.listing.Listing
 import com.sthoray.allright.data.model.main.FeatureCategoriesResponse
 import com.sthoray.allright.data.model.search.SearchResponse
+import com.sthoray.allright.ui.listing.viewmodel.ListingViewModel
+import com.sthoray.allright.utils.Internet
+import com.sthoray.allright.utils.Resource
+import com.sthoray.allright.utils.TestCoroutineRule
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.experimental.categories.Category
+import org.junit.rules.TestRule
+import retrofit2.Response
 
 
 class AppRepositoryTest {
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = TestCoroutineRule()
 
     @MockK
     private lateinit var historyDatabase: SearchHistoryDatabase
@@ -22,9 +42,40 @@ class AppRepositoryTest {
     private lateinit var searchListingResponse: SearchResponse
     @RelaxedMockK
     private lateinit var getListingResponse: Listing
-    @Test
-    fun getFeatureCategories() {
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        mockkObject(Internet)
     }
+    @Test
+    fun getFeaturedCategories() =
+        mainCoroutineRule.runBlockingTest {
+            val appRepository = AppRepository(historyDatabase)
+            val response = Response.success(featureCategoriesResponse)
+
+            coEvery { RetrofitInstance.api.getFeatureCategories() } returns response
+            appRepository.getFeatureCategories()
+            coVerify { RetrofitInstance.api.getFeatureCategories() }
+
+            /*val testId = 9
+            val errorResponse: Response<Listing> = Response.error(
+                400,
+                "{\"key\":[\"some_stuff\"]}"
+                    .toResponseBody("application/json".toMediaTypeOrNull())
+            )
+
+            every { Internet.hasConnection(any()) } returns true
+            coEvery { appRepository.getListing(eq(testId)) } returns errorResponse
+
+            val listingViewModel = ListingViewModel(app, appRepository)
+            listingViewModel.getListing(testId)
+
+            verify { Internet.hasConnection(any()) }
+
+            Truth.assertThat(listingViewModel.listing.value).isInstanceOf(Resource.Error::class.java)
+            Truth.assertThat(listingViewModel.listing.value?.message).isEqualTo(errorResponse.message())*/
+        }
 
     @Test
     fun getSecondTierCategories() {
