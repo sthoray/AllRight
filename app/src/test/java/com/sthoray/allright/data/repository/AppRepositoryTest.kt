456af0f8bdb5b5a761add81fb0236fc4e0538e2e
Usman Shah <shaus019@student.otago.ac.nz>
2020-08-09 19:53:43 +1200

    added a class to test AppRepository

---
+++ app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
@@ -0,0 +1,24 @@
+package com.sthoray.allright.data.repository
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AppRepositoryTest {
+
+    @Test
+    fun getFeatureCategories() {
+    }
+
+    @Test
+    fun getSecondTierCategories() {
+    }
+
+    @Test
+    fun searchListings() {
+    }
+
+    @Test
+    fun getListing() {
+    }
+}
