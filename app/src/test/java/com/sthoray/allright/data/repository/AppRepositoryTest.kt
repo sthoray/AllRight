7788e48947e85c68119eec8af9eb24f706ce1e6b
Usman Shah <shaus019@student.otago.ac.nz>
2020-08-09 20:20:36 +1200

    created mocked objects

--- app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
+++ app/src/test/java/com/sthoray/allright/data/repository/AppRepositoryTest.kt
@@ -1,11 +1,27 @@
 package com.sthoray.allright.data.repository
 
+import com.sthoray.allright.data.db.SearchHistoryDatabase
+import com.sthoray.allright.data.model.listing.Listing
+import com.sthoray.allright.data.model.main.FeatureCategoriesResponse
+import com.sthoray.allright.data.model.search.SearchResponse
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Test
+import org.junit.experimental.categories.Category
 
-import org.junit.Assert.*
 
 class AppRepositoryTest {
 
+    @MockK
+    private lateinit var historyDatabase: SearchHistoryDatabase
+    @RelaxedMockK
+    private lateinit var featureCategoriesResponse: FeatureCategoriesResponse
+    @RelaxedMockK
+    private lateinit var secondTierCategoriesResponse : List<Category>
+    @RelaxedMockK
+    private lateinit var searchListingResponse: SearchResponse
+    @RelaxedMockK
+    private lateinit var getListingResponse: Listing
     @Test
     fun getFeatureCategories() {
     }
