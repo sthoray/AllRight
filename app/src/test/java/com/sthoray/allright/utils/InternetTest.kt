3cd4e92d94264d1087c2f227ac95acd37d2c12ba
Josh Mullin <jmull0@protonmail.com>
2020-08-09 12:56:14 +1200

    Add skeleton of first getInternetTest, init MockKAnnotations

--- app/src/test/java/com/sthoray/allright/utils/InternetTest.kt
+++ app/src/test/java/com/sthoray/allright/utils/InternetTest.kt
@@ -1,14 +1,21 @@
 package com.sthoray.allright.utils
 
+import android.app.Application
+import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Before
-import kotlin.properties.Delegates
+import org.junit.Test
 
 class InternetTest {
     @RelaxedMockK
-    var deviceApi by Delegates.notNull<Int>()
-
+    lateinit var app : Application
     @Before
     fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun getInternet_API_above_M_success_returns_true(){
+
     }
 }
