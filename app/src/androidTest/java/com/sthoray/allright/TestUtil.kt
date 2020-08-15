30a02a79bbdbe0bc8a73b8749542eafcaea59ad9
Sebastian Sole <scsole@protonmail.com>
2020-08-16 08:24:35 +1200

    Add TestUtil class
    This class should also be accessible for local tests so needs to be moved later

---
+++ app/src/androidTest/java/com/sthoray/allright/TestUtil.kt
@@ -0,0 +1,11 @@
+package com.sthoray.allright
+
+import com.sthoray.allright.data.model.search.SearchRequest
+
+class TestUtil {
+    companion object {
+        fun createSearchRequest(): SearchRequest {
+            return SearchRequest()
+        }
+    }
+}
