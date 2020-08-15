c950d0d206597e34e84bec979dc3dcd0af223028
Sebastian Sole <scsole@protonmail.com>
2020-08-16 11:33:58 +1200

    Add remaining DAO unit tests

--- app/src/androidTest/java/com/sthoray/allright/data/db/SearchHistoryDatabaseTest.kt
+++ app/src/androidTest/java/com/sthoray/allright/data/db/SearchHistoryDatabaseTest.kt
@@ -10,6 +10,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import com.sthoray.allright.TestUtil
 import com.sthoray.allright.data.model.search.SearchRequest
+import com.sthoray.allright.utils.SortOrder
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
@@ -46,14 +47,135 @@ class SearchHistoryDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun writeSearchRequestAndReadInList() = runBlocking<Unit> {
+    fun writeSearchRequest_readInHistory() = runBlocking<Unit> {
         val searchRequest: SearchRequest = TestUtil.createSearchRequest().apply {
             categoryId = 3
         }
         searchHistoryDao.upsert(searchRequest)
-        val history = searchHistoryDao.getHistory()
+        val history = searchHistoryDao.getHistory().blockingObserve()
 
-        assertThat(history.blockingObserve()?.get(0)).isEqualTo(searchRequest)
+        assertThat(history?.get(0)).isEqualTo(searchRequest)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeMultipleSearchRequests_readInHistory() = runBlocking<Unit> {
+        val searchRequest1: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 0
+        }
+        val searchRequest2: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 1
+        }
+        val searchRequest3: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 2
+        }
+        searchHistoryDao.upsert(searchRequest1)
+        searchHistoryDao.upsert(searchRequest2)
+        searchHistoryDao.upsert(searchRequest3)
+        val history = searchHistoryDao.getHistory().blockingObserve()
+
+        assertThat(history?.size).isEqualTo(3)
+        assertThat(history?.get(0)).isEqualTo(searchRequest1)
+        assertThat(history?.get(1)).isEqualTo(searchRequest2)
+        assertThat(history?.get(2)).isEqualTo(searchRequest3)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeDuplicateSearchRequest_readNoDuplicatesInHistory() = runBlocking<Unit> {
+        val searchRequest: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 3
+        }
+        searchHistoryDao.upsert(searchRequest)
+        searchHistoryDao.upsert(searchRequest)
+        val history = searchHistoryDao.getHistory().blockingObserve()
+
+        assertThat(history?.size).isEqualTo(1)
+        assertThat(history?.get(0)).isEqualTo(searchRequest)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun updateSearchRequest_readUpdatedInHistory() = runBlocking<Unit> {
+        val searchRequest: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 3
+        }
+        searchHistoryDao.upsert(searchRequest)
+        searchRequest.sortBy = SortOrder.ALPHABETICAL.key
+        searchHistoryDao.upsert(searchRequest)
+        val history = searchHistoryDao.getHistory().blockingObserve()
+
+        assertThat(history?.size).isEqualTo(1)
+        assertThat(history?.get(0)).isEqualTo(searchRequest)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeThenDeleteSearchRequest_readNothingInHistory() = runBlocking<Unit> {
+        val searchRequest: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 3
+        }
+        searchHistoryDao.upsert(searchRequest)
+        searchHistoryDao.delete(searchRequest)
+        val history = searchHistoryDao.getHistory().blockingObserve()
+
+        assertThat(history?.size).isEqualTo(0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeMultipleSearchRequestsThenDeleteOne_readRemainderInHistory() = runBlocking<Unit> {
+        val searchRequest1: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 0
+        }
+        val searchRequest2: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 1
+        }
+        val searchRequest3: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 2
+        }
+        searchHistoryDao.upsert(searchRequest1)
+        searchHistoryDao.upsert(searchRequest2)
+        searchHistoryDao.upsert(searchRequest3)
+        searchHistoryDao.delete(searchRequest2)
+        val history = searchHistoryDao.getHistory().blockingObserve()
+
+        assertThat(history?.size).isEqualTo(2)
+        assertThat(history?.get(0)).isEqualTo(searchRequest1)
+        assertThat(history?.get(1)).isEqualTo(searchRequest3)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun deleteSearchRequestFromEmptyTable_readNothingInHistory() = runBlocking<Unit> {
+        val searchRequest: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 3
+        }
+        searchHistoryDao.delete(searchRequest)
+        val history = searchHistoryDao.getHistory().blockingObserve()
+
+        assertThat(history?.size).isEqualTo(0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeMultipleSearchRequestsThenClearHistory_readNothingInHistory() = runBlocking<Unit> {
+        val searchRequest1: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 0
+        }
+        val searchRequest2: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 1
+        }
+        val searchRequest3: SearchRequest = TestUtil.createSearchRequest().apply {
+            categoryId = 2
+        }
+        searchHistoryDao.upsert(searchRequest1)
+        searchHistoryDao.upsert(searchRequest2)
+        searchHistoryDao.upsert(searchRequest3)
+        searchHistoryDao.clearHistory()
+        val history = searchHistoryDao.getHistory().blockingObserve()
+
+        assertThat(history?.size).isEqualTo(0)
     }
 
     private fun <T> LiveData<T>.blockingObserve(): T? {
