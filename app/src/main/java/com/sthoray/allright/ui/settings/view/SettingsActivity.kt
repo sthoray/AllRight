f45b7f076af8f659040b46f9062669f998935f15
Sebastian Sole <scsole@protonmail.com>
2020-09-12 16:18:48 +1200

    Add option to logout in settings

--- app/src/main/java/com/sthoray/allright/ui/settings/view/SettingsActivity.kt
+++ app/src/main/java/com/sthoray/allright/ui/settings/view/SettingsActivity.kt
@@ -3,7 +3,10 @@ package com.sthoray.allright.ui.settings.view
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import com.sthoray.allright.R
 
 /**
@@ -52,12 +55,41 @@ class SettingsActivity : AppCompatActivity() {
          * Inflate the hierarchy from an XML resource.
          *
          * @param savedInstanceState If the fragment is being re-created from a previous
-         * savedk state, this is the state.
+         * saved state, this is the state.
          * @param rootKey If non-null, this preference fragment should be rooted at the
          * PreferenceScreen with this key.
          */
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val logoutPreference: Preference? = findPreference("logout")
+            logoutPreference?.setOnPreferenceClickListener {
+                logout()
+                true
+            }
+        }
+
+        private fun logout() {
+            context?.let { context ->
+                val masterKeyAlias = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build()
+
+                val encryptedSharedPreferences = EncryptedSharedPreferences.create(
+                    context,
+                    getString(R.string.preference_crypt_auth_key),
+                    masterKeyAlias,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                )
+
+                encryptedSharedPreferences.edit().apply {
+                    putString(
+                        getString(R.string.user_bearer_token_key),
+                        ""
+                    )
+                }.apply()
+            }
         }
     }
 }
