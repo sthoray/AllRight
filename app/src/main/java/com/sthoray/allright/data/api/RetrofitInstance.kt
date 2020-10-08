440f6a4883121da6348ee05b34af7ebea95d2585
Sebastian Sole <scsole@protonmail.com>
2020-10-09 08:37:40 +1300

    Make retrofit responses mockable

--- app/src/main/java/com/sthoray/allright/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/sthoray/allright/data/api/RetrofitInstance.kt
@@ -2,6 +2,8 @@ package com.sthoray.allright.data.api
 
 import com.sthoray.allright.BuildConfig
 import com.sthoray.allright.utils.Constants.Companion.BASE_API_URL
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,13 +15,26 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitInstance {
     companion object {
 
+        /**
+         * The base url for the retrofit builder.
+         *
+         * If this variable is modified before the retrofit builder is created, i.e. when
+         * [RetrofitInstance.api] is first accessed, then the modified baseUrl will be used.
+         * If the retrofit builder object has already been initialised, then modifying this
+         * variable will have no affect on retrofit calls. This should be set to a
+         * `mockWebServer.url("/")` when mocking retrofit network calls in tests. We
+         * recommend making this variable private in a later refactor.
+         */
+        var baseUrl: HttpUrl = BASE_API_URL.toHttpUrl()
+
         private val retrofit by lazy {
             // Logging interceptor to make debugging easier.
             val logging = HttpLoggingInterceptor()
-            val loggingLevel =
+            logging.setLevel(
                 if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
                 else HttpLoggingInterceptor.Level.NONE
-            logging.setLevel(loggingLevel)
+            )
+
             // OKHttp client with logging interceptor
             val client = OkHttpClient.Builder()
                 .addInterceptor(logging)
@@ -27,13 +42,17 @@ class RetrofitInstance {
 
             // Finally, the retrofit builder with Gson converter for deserialization
             Retrofit.Builder()
-                .baseUrl(BASE_API_URL)
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
         }
 
-        /** The actual API service object that we can use for requests. */
+        /**
+         * The actual API service object for making network requests.
+         *
+         * Implements the [AllGoodsApi] interface.
+         */
         val api: AllGoodsApi by lazy {
             retrofit.create(AllGoodsApi::class.java)
         }
